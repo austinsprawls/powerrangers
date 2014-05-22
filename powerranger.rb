d62df0a412d7bd48d790c027e125521799fc61fd
Austin Sprawls <austinsprawls@gmail.com>
2014-05-22 11:43:32 -0500

    Added scream, run and drink_coffee methods to Person class

--- powerranger.rb
+++ powerranger.rb
@@ -4,7 +4,17 @@ class Person
         @caffeine_level = caffeine_level
     end
 
-    
+    def run
+        puts "Making an escape now"
+    end
+
+    def scream
+        puts "AHHHH!"
+    end
+
+    def drink_coffee
+        @caffeine_level += 1
+    end
 end
 
 class PowerRanger
