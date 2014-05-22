82267f8d9caf292f24f8f924105fa146f97a331f
Austin Sprawls <austinsprawls@gmail.com>
2014-05-22 11:59:00 -0500

    Added punch and use_megazord methods to PowerRanger class

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,7 @@
 class Person
+
+    attr_accessor :name, :caffeine_level
+
     def initialize(name, caffeine_level)
         @name = name
         @caffeine_level = caffeine_level
@@ -19,11 +22,32 @@ end
 
 class PowerRanger < Person
 
-    def initialize(strength, color)
+    def initialize(name, caffeine_level, strength, color)
+        @name = name
+        @caffeine_level = caffeine_level
         @strength = strength
         @color = color
     end
+
+    def punch(person, power)
+        if power < 5
+            puts "#{person.name} was punched with a power level of #{power}! #{person.scream} #{person.run}"
+            person.caffeine_level -= 1
+        else
+            puts "#{person.name} was somersaulted into the air with a punch power level of #{power}!"
+            person.caffeine_level -= 5
+        end
+
+    end
+
+    def rest
+    end
+
+    def user_megazord(person)
+        puts "#{person.name} was hit with the MegaZord!"
+        person.caffeine_level -= 10
+    end
 end
 
-class EvilNinja
+class EvilNinja < Person
 end
